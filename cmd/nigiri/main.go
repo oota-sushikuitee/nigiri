91ef0334b32eda3bc4f0ab9e5ad4fdbcd234ca5c
Junya Okabe <okabe.junya.qj@alumni.tsukuba.ac.jp>
2024-05-11 04:36:55 +0900

    update: main.go

--- cmd/nigiri/main.go
+++ cmd/nigiri/main.go
@@ -10,6 +10,31 @@ import (
 func main() {
 	app := &cli.App{
 		Name: "nigiri",
+		Commands: []*cli.Command{
+			{
+				Name:  "build",
+				Usage: "TODO: Write usage",
+				Action: func(c *cli.Context) error {
+					log.Println("build")
+					return nil
+				},
+			}, {
+				Name:  "run",
+				Usage: "TODO: Write usage",
+				Action: func(c *cli.Context) error {
+					log.Println("run")
+					return nil
+				},
+			}, {
+				Name:    "remove",
+				Aliases: []string{"rm"},
+				Usage:   "TODO: Write usage",
+				Action: func(c *cli.Context) error {
+					log.Println("rm")
+					return nil
+				},
+			},
+		},
 	}
 
 	if err := app.Run(os.Args); err != nil {
